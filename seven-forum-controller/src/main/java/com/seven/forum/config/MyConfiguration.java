f37312b3b53492c35cef5bb8dc060ac933055125
lihaijian <1595606639@qq.com>
2022-08-18 17:45:44 +0800

    dynamic

---
+++ seven-forum-controller/src/main/java/com/seven/forum/config/MyConfiguration.java
@@ -0,0 +1,23 @@
+package com.seven.forum.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyConfiguration implements WebMvcConfigurer {
+
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/api/**").allowedMethods("*");
+//    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:8848")
+                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("http://localhost:8081")
+                .allowedMethods("GET","POST","OPTIONS").allowCredentials(false);
+    }
+}
